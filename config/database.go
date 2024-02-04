a732a749ef43431236ddf8570efe74c2712ee95b
King Lawrence Caubalejo <ecarga-renz@CARGA-NC-022.local>
2024-02-05 00:20:47 +0800

    feat: for release in github.com

--- config/database.go
+++ config/database.go
@@ -2,6 +2,6 @@ package config
 
 var DB_HOST string = "localhost"
 var DB_PORT string = "3306"
-var DB_USER string = "yc_admin_dev"
-var DB_PASSWORD string = "Pa55w0rd_d3v"
-var DB_NAME string = "yc_admin_db_dev"
+var DB_USER string = "<your_db_user>"
+var DB_PASSWORD string = "<your_db_password>"
+var DB_NAME string = "<your_db_name>"
